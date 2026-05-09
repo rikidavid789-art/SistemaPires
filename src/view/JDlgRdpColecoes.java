b126bd7a7da5842ae21c78042a5e576a325585e5
rikidavid789-art <rikidavid789@gmail.com>
2026-05-09 19:43:31 -0300

    FizFiz a referenciação da tela de coleções n

--- src/view/JDlgRdpColecoes.java
+++ src/view/JDlgRdpColecoes.java
@@ -18,8 +18,54 @@ public class JDlgRdpColecoes extends javax.swing.JDialog {
     public JDlgRdpColecoes(java.awt.Frame parent, boolean modal) {
         super(parent, modal);
         initComponents();
+          setTitle("Loja de Roupas do Rick - Coleção");
+            setLocationRelativeTo(null);
+            jTxtRdpCategoria.setEnabled(false);
+            jTxtRdpDataLancamento.setEnabled(false);
+            jTxtRdpDescricao.setEnabled(false);
+            jTxtRdpEstacao.setEnabled(false);
+            jTxtRdpID.setEnabled(false);
+            jCboRdpStatus.setEnabled(false);
+            jTxtRdpNomeColecao.setEnabled(false);
+            jBtnRdpCancelar.setEnabled(false);
+            jBtnRdpConfirmar.setEnabled(false);
+            jBtnRdpAlterar.setEnabled(true);
+            jBtnRdpExcluir.setEnabled(true);
+            jBtnRdpIncluir.setEnabled(true);
+            jBtnRdpPesquisar.setEnabled(true);
+    }
+    
+      public void habilitar(){
+            jTxtRdpCategoria.setEnabled(false);
+            jTxtRdpDataLancamento.setEnabled(false);
+            jTxtRdpDescricao.setEnabled(false);
+            jTxtRdpEstacao.setEnabled(false);
+            jTxtRdpID.setEnabled(false);
+            jCboRdpStatus.setEnabled(false);
+            jTxtRdpNomeColecao.setEnabled(false);
+            jBtnRdpCancelar.setEnabled(true);
+            jBtnRdpConfirmar.setEnabled(true);
+            jBtnRdpAlterar.setEnabled(false);
+            jBtnRdpExcluir.setEnabled(false);
+            jBtnRdpIncluir.setEnabled(false);
+            jBtnRdpPesquisar.setEnabled(false); 
     }
 
+         public void desabilitar(){
+              jTxtRdpCategoria.setEnabled(false);
+            jTxtRdpDataLancamento.setEnabled(false);
+            jTxtRdpDescricao.setEnabled(false);
+            jTxtRdpEstacao.setEnabled(false);
+            jTxtRdpID.setEnabled(false);
+            jCboRdpStatus.setEnabled(false);
+            jTxtRdpNomeColecao.setEnabled(false);
+            jBtnRdpCancelar.setEnabled(false);
+            jBtnRdpConfirmar.setEnabled(false);
+            jBtnRdpAlterar.setEnabled(true);
+            jBtnRdpExcluir.setEnabled(true);
+            jBtnRdpIncluir.setEnabled(true);
+            jBtnRdpPesquisar.setEnabled(true);
+         }
     /**
      * This method is called from within the constructor to initialize the form.
      * WARNING: Do NOT modify this code. The content of this method is always
@@ -29,21 +75,193 @@ public class JDlgRdpColecoes extends javax.swing.JDialog {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        jLabel1 = new javax.swing.JLabel();
+        jLabel6 = new javax.swing.JLabel();
+        jLabel2 = new javax.swing.JLabel();
+        jTxtRdpID = new javax.swing.JTextField();
+        jLabel3 = new javax.swing.JLabel();
+        jTxtRdpNomeColecao = new javax.swing.JTextField();
+        jLabel4 = new javax.swing.JLabel();
+        jTxtRdpDescricao = new javax.swing.JTextField();
+        jLabel5 = new javax.swing.JLabel();
+        jTxtRdpCategoria = new javax.swing.JTextField();
+        jLabel7 = new javax.swing.JLabel();
+        jTxtRdpEstacao = new javax.swing.JTextField();
+        jLabel8 = new javax.swing.JLabel();
+        jTxtRdpDataLancamento = new javax.swing.JTextField();
+        jCboRdpStatus = new javax.swing.JComboBox<>();
+        jLabel9 = new javax.swing.JLabel();
+        jBtnRdpIncluir = new javax.swing.JButton();
+        jBtnRdpAlterar = new javax.swing.JButton();
+        jBtnRdpExcluir = new javax.swing.JButton();
+        jBtnRdpConfirmar = new javax.swing.JButton();
+        jBtnRdpCancelar = new javax.swing.JButton();
+        jBtnRdpPesquisar = new javax.swing.JButton();
+
+        jLabel1.setText("jLabel1");
+
+        jLabel6.setText("jLabel6");
+
         setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
+
+        jLabel2.setText("ID");
+
+        jLabel3.setText("Nome da Coleção");
+
+        jLabel4.setText("Descrição");
+
+        jLabel5.setText("Categoria");
+
+        jLabel7.setText("Estação");
+
+        jLabel8.setText("Data de Lançamento");
+
+        jCboRdpStatus.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Item 1", "Item 2", "Item 3", "Item 4" }));
+
+        jLabel9.setText("Status");
+
+        jBtnRdpIncluir.setIcon(new javax.swing.ImageIcon(getClass().getResource("/imagens/incluir.png"))); // NOI18N
+        jBtnRdpIncluir.setText("Incluir");
+        jBtnRdpIncluir.addActionListener(this::jBtnRdpIncluirActionPerformed);
+
+        jBtnRdpAlterar.setIcon(new javax.swing.ImageIcon(getClass().getResource("/imagens/alterar.png"))); // NOI18N
+        jBtnRdpAlterar.setText("Alterar");
+
+        jBtnRdpExcluir.setIcon(new javax.swing.ImageIcon(getClass().getResource("/imagens/Excluir.png"))); // NOI18N
+        jBtnRdpExcluir.setText("Excluir");
+        jBtnRdpExcluir.addActionListener(this::jBtnRdpExcluirActionPerformed);
+
+        jBtnRdpConfirmar.setIcon(new javax.swing.ImageIcon(getClass().getResource("/imagens/confirmar.png"))); // NOI18N
+        jBtnRdpConfirmar.setText("Confirmar");
+        jBtnRdpConfirmar.addActionListener(this::jBtnRdpConfirmarActionPerformed);
+
+        jBtnRdpCancelar.setIcon(new javax.swing.ImageIcon(getClass().getResource("/imagens/cancelar.png"))); // NOI18N
+        jBtnRdpCancelar.setText("Cancelar");
+        jBtnRdpCancelar.addActionListener(this::jBtnRdpCancelarActionPerformed);
+
+        jBtnRdpPesquisar.setIcon(new javax.swing.ImageIcon(getClass().getResource("/imagens/pesquisar.png"))); // NOI18N
+        jBtnRdpPesquisar.setText("Pesquisar");
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addComponent(jTxtRdpID, javax.swing.GroupLayout.PREFERRED_SIZE, 31, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                        .addComponent(jTxtRdpNomeColecao, javax.swing.GroupLayout.PREFERRED_SIZE, 232, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(jTxtRdpDescricao, javax.swing.GroupLayout.PREFERRED_SIZE, 266, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addGroup(layout.createSequentialGroup()
+                        .addComponent(jLabel5)
+                        .addGap(166, 166, 166)
+                        .addComponent(jLabel7))
+                    .addGroup(layout.createSequentialGroup()
+                        .addComponent(jTxtRdpCategoria, javax.swing.GroupLayout.PREFERRED_SIZE, 210, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(jTxtRdpEstacao, javax.swing.GroupLayout.PREFERRED_SIZE, 224, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                .addGap(0, 115, Short.MAX_VALUE))
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addComponent(jLabel2)
+                        .addGap(27, 27, 27)
+                        .addComponent(jLabel3)
+                        .addGap(143, 143, 143)
+                        .addComponent(jLabel4))
+                    .addGroup(layout.createSequentialGroup()
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(jTxtRdpDataLancamento, javax.swing.GroupLayout.PREFERRED_SIZE, 204, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(jLabel8))
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(jLabel9)
+                            .addComponent(jCboRdpStatus, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))))
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+            .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                .addGroup(layout.createSequentialGroup()
+                    .addContainerGap()
+                    .addComponent(jBtnRdpIncluir)
+                    .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                    .addComponent(jBtnRdpAlterar)
+                    .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                    .addComponent(jBtnRdpExcluir)
+                    .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                    .addComponent(jBtnRdpConfirmar, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                    .addComponent(jBtnRdpCancelar)
+                    .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                    .addComponent(jBtnRdpPesquisar)
+                    .addContainerGap()))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabel2)
+                    .addComponent(jLabel3, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addComponent(jLabel4))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jTxtRdpID, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jTxtRdpNomeColecao, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jTxtRdpDescricao, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabel5)
+                    .addComponent(jLabel7))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jTxtRdpCategoria, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jTxtRdpEstacao, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabel8)
+                    .addComponent(jLabel9))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jTxtRdpDataLancamento, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jCboRdpStatus, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addContainerGap(273, Short.MAX_VALUE))
+            .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                .addGroup(layout.createSequentialGroup()
+                    .addGap(199, 199, 199)
+                    .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                        .addComponent(jBtnRdpIncluir)
+                        .addComponent(jBtnRdpAlterar)
+                        .addComponent(jBtnRdpExcluir)
+                        .addComponent(jBtnRdpConfirmar, javax.swing.GroupLayout.PREFERRED_SIZE, 31, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addComponent(jBtnRdpCancelar)
+                        .addComponent(jBtnRdpPesquisar))
+                    .addContainerGap(199, Short.MAX_VALUE)))
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void jBtnRdpIncluirActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jBtnRdpIncluirActionPerformed
+        // TODO add your handling code here:
+        habilitar();
+    }//GEN-LAST:event_jBtnRdpIncluirActionPerformed
+
+    private void jBtnRdpExcluirActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jBtnRdpExcluirActionPerformed
+        // TODO add your handling code here:
+        desabilitar();
+    }//GEN-LAST:event_jBtnRdpExcluirActionPerformed
+
+    private void jBtnRdpConfirmarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jBtnRdpConfirmarActionPerformed
+        // TODO add your handling code here:
+        habilitar();
+    }//GEN-LAST:event_jBtnRdpConfirmarActionPerformed
+
+    private void jBtnRdpCancelarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jBtnRdpCancelarActionPerformed
+        // TODO add your handling code here:
+        habilitar();
+    }//GEN-LAST:event_jBtnRdpCancelarActionPerformed
 
     /**
      * @param args the command line arguments
@@ -83,5 +301,27 @@ public class JDlgRdpColecoes extends javax.swing.JDialog {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton jBtnRdpAlterar;
+    private javax.swing.JButton jBtnRdpCancelar;
+    private javax.swing.JButton jBtnRdpConfirmar;
+    private javax.swing.JButton jBtnRdpExcluir;
+    private javax.swing.JButton jBtnRdpIncluir;
+    private javax.swing.JButton jBtnRdpPesquisar;
+    private javax.swing.JComboBox<String> jCboRdpStatus;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel3;
+    private javax.swing.JLabel jLabel4;
+    private javax.swing.JLabel jLabel5;
+    private javax.swing.JLabel jLabel6;
+    private javax.swing.JLabel jLabel7;
+    private javax.swing.JLabel jLabel8;
+    private javax.swing.JLabel jLabel9;
+    private javax.swing.JTextField jTxtRdpCategoria;
+    private javax.swing.JTextField jTxtRdpDataLancamento;
+    private javax.swing.JTextField jTxtRdpDescricao;
+    private javax.swing.JTextField jTxtRdpEstacao;
+    private javax.swing.JTextField jTxtRdpID;
+    private javax.swing.JTextField jTxtRdpNomeColecao;
     // End of variables declaration//GEN-END:variables
 }
